124db8073fe86fe599f6118a136f9cef81f452c8
zanman <leoarmand@orange.fr>
2025-07-01 16:35:05 +0200

    added scroll to AnimalDetailsScreen

--- app/src/main/java/com/animals/safety/screens/AnimalDetailsScreen.kt
+++ app/src/main/java/com/animals/safety/screens/AnimalDetailsScreen.kt
@@ -6,9 +6,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -127,7 +130,9 @@ private fun AnimalDetails(
   animal: Animal,
 )
 {
-  Column (modifier = modifier) {
+  val scrollState = rememberScrollState()
+
+  Column (modifier = modifier.verticalScroll(scrollState)) {
     Row {
         AnimalDetailsHeader(animal.name, animal.breed)
     }
